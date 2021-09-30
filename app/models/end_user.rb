4a52c796cb5464c41a2752516bc064f39f30f80b
EC2 Default User <ec2-user@ip-172-31-13-172.us-east-2.compute.internal>
2021-09-30 10:56:23 +0000

    認証機能の実装

---
+++ app/models/end_user.rb
@@ -0,0 +1,9 @@
+class EndUser < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :phone_number, presence: true
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,34 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'homes#top'
+  get 'about' => 'homes#about'
+
+
+  devise_for :admin, controllers: {
+    sessions: 'admin/sessions',
+    passwords: 'admin/passwords',
+    registrations: 'admin/registration'
+  }
+
+  devise_for :end_users, controllers: {
+    sessions: 'end_users/sessions',
+    passwords: 'end_users/passwords',
+    registrations: 'end_users/registrations'
+  }
+
+  resources :end_users, only: [:show, :edit, :update, :destroy] do
+    member do
+      get :quit_confirm
+    end
+    patch "/hide" => "end_users#hide", as: 'hide'
+  end
+
+  resources :items, only: [:index, :show]
+
+  namespace :admin do
+    get '/', to: 'homes#top'
+    resources :end_users, only: [:index]
+    resources :items, except: [:destroy]
+  end
+
 end
